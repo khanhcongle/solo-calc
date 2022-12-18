4595786c2a71ea19bca232aed87c998fd3198d69
khanhcongle <lecongkhanh382@gmail.com>
2022-12-18 22:28:57 +0700

    create app

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,70 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import TestResults from './lib/TestResults.svelte'
+
+  function createRandomTest(id) {
+    let num1 = getRandomInteger(1, 101);
+    let symbol = getRandomFomular()
+    let test = `${num1} ${symbol} ${getRandomIntegerBySymbol(symbol)}`;
+    let expected = eval(test);
+    let actual = undefined;
+    let isCorrect = undefined;
+    let result = {id, test, expected, isCorrect };
+    console.log(result);
+    return result;
+  }
+  function getRandomFomular() {
+    let num = getRandomInteger(0, 4);
+    switch(num) {
+      case 0:
+        return "+";
+      case 1:
+        return "-";
+      case 2:
+        return "*";
+      default:
+        return "-";
+    }
+  }
+
+  function getRandomIntegerBySymbol(symbol) {
+    switch(symbol) {
+      case "*":
+      case "/":
+        return getRandomInteger(2, 11);
+      default:
+        return getRandomInteger(1, 101);
+    }
+  }
+
+  function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+  let currentTest = createRandomTest(0);
+
+  let results = [];
+  function onSubmit(event) {
+    currentTest.actual = event.target['answer'].value;
+    currentTest.isCorrect = eval(currentTest.expected) == eval(currentTest.actual);
+    results.unshift(currentTest);
+
+    results = results;
+
+    event.target['answer'].value = '';
+    currentTest = createRandomTest(results.length);
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
   <div class="card">
-    <Counter />
+    <form on:submit|preventDefault={onSubmit}>
+      <label for="answer">{currentTest.test} = </label>
+      <input id="answer" name="answer">
+      <TestResults {results} />
+    </form>
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
---
+++ src/lib/TestResults.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let results;
+</script>
+
+<table>
+  <tr>
+    <th>id</th>
+    <th>test</th>
+    <th>expected</th>
+    <th>actual</th>
+    <th>result</th>
+  </tr>
+	{#each results as { id, test, expected, actual, isCorrect }, i}
+		<tr>
+      <td>{id}</td>
+      <td>{test}</td>
+      <td>{expected}</td>
+      <td>{actual}</td>
+      <td>{isCorrect}</td>
+    </tr>
+	{/each}
+</table>
+
+<style>
+td, th {
+  border: 1px solid #ddd;
+  padding: 8px;
+  min-width: 68px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2;}
+
+tr:hover {background-color: #ddd;}
+
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
